b013bc011cf7ca97381c4c575d754ae5a3853130
Adolfo Eloy <1496547+adolfoweloy@users.noreply.github.com>
2020-12-05 00:20:19 +1100

    refactor backtracking algorithm

--- day01.py
+++ day01.py
@@ -3,19 +3,22 @@ from functools import reduce
 from typing import List
 
 
-def find_numbers(candidates: List[int], combination: List[int], sum: int, start: int, target: int, acc: List[int]):
+# the solution for this problem solves both part 1 and part 2
+# the parameter size is what makes the difference here for part1 and part 2
+def find_numbers(candidates: List[int], combination: List[int], total_sum: int, start: int, target: int, acc: List[int],
+                 size: int):
     for i in range(start, len(candidates)):
         # boundary for this depth-first search
-        if sum > target or len(acc) > 0:
+        if total_sum > target or len(acc) > 0:
             break
 
         # backtracking
         combination.append(candidates[i])
-        find_numbers(candidates, combination, sum + candidates[i], i + 1, target, acc)
+        find_numbers(candidates, combination, total_sum + candidates[i], i + 1, target, acc, size)
         combination.pop()
 
         # saving the result when sum == target (I don't like this solution)
-        if sum == target:
+        if total_sum == target and size == len(combination):
             for c in combination:
                 acc.append(c)
             return
@@ -31,6 +34,6 @@ if __name__ == '__main__':
         for line in reader.readlines():
             nums.append(int(line))
 
-    find_numbers(nums, [], 0, 0, 2020, result)
+    find_numbers(nums, [], 0, 0, 2020, result, 2)
 
-    print(reduce(lambda a,b: a * b, result))
+    print(reduce(lambda a, b: a * b, result))
