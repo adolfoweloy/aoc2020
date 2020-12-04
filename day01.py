f5e5aa8121b7aa817d6ae9ffcf92f1e4ff5c9f39
Adolfo Eloy <1496547+adolfoweloy@users.noreply.github.com>
2020-12-05 00:16:53 +1100

    refactor backtracking algorithm

--- day01.py
+++ day01.py
@@ -1,44 +1,36 @@
-from functools import reduce
-from typing import List, Tuple
 import sys
+from functools import reduce
+from typing import List
 
 
-# map the indexes with True or False which means if that index can be used to add up to 2020
-# this algorithm uses backtracking with depth-first technique to find the sum
-def map_indexes(values: List[int], idx: int, node: Tuple[int, int], acc: List[bool]) -> Tuple[List[bool], bool]:
-    if node[0] > 2020 or node[1] <= 0:
-        return acc, False
-    elif node[0] == 2020 and len(list(filter(lambda x: x, acc))) == 3:
-        return acc, True
-    else:
-        left = map_indexes(values, idx + 1, (node[0] + values[idx], node[1] - values[idx]), acc + [True])
-        if left[1]:
-            return left[0], True
-        else:
-            right = map_indexes(values, idx + 1, (node[0], node[1] - values[idx]), acc + [False])
-            if right[1]:
-                return right[0], True
-            else:
-                acc.pop()
-                return acc, False
+def find_numbers(candidates: List[int], combination: List[int], sum: int, start: int, target: int, acc: List[int]):
+    for i in range(start, len(candidates)):
+        # boundary for this depth-first search
+        if sum > target or len(acc) > 0:
+            break
+
+        # backtracking
+        combination.append(candidates[i])
+        find_numbers(candidates, combination, sum + candidates[i], i + 1, target, acc)
+        combination.pop()
+
+        # saving the result when sum == target (I don't like this solution)
+        if sum == target:
+            for c in combination:
+                acc.append(c)
+            return
 
 
 if __name__ == '__main__':
     path = sys.argv[1]
-    file_handle = open(path, 'r')
-    line = file_handle.readline()
     nums: List[int] = []
+    result: List[int] = []
 
-    while line:
-        nums.append(int(line))
-        line = file_handle.readline()
+    # collecting the lines
+    with open(path, 'r') as reader:
+        for line in reader.readlines():
+            nums.append(int(line))
 
-    total = reduce(lambda a, b: a + b, nums)
-    index_map = map_indexes(nums, 0, (0, total), [])
-    result = []
+    find_numbers(nums, [], 0, 0, 2020, result)
 
-    for i in range(0, len(nums)):
-        if i < len(index_map[0]) and index_map[0][i]:
-            result.append(nums[i])
-
-    print(reduce(lambda a, b: a * b, result))
+    print(reduce(lambda a,b: a * b, result))
